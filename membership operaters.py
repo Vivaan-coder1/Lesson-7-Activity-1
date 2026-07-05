f096c2c9cc007bf145130959ee970ed8e12f157f
Vivaan-coder1 <anubhaasthana13@gmail.com>
2026-07-05 12:35:18 +0530

    Add files via upload

---
+++ membership operaters.py
@@ -0,0 +1,20 @@
+x = 5
+if (type(x) is int):
+    print("True")
+else:
+    print("False")
+
+x = 5.5
+if (type(x) is not float):
+    print("True")
+else:
+    print("False")
+
+x = 20
+y = 20
+if (x is y):
+    print("x & y are SAME identity")
+
+y = 30
+if (x is not y):
+    print("x & y have DIFFERENT identity ")
